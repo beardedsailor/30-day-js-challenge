f30b924f7123fb779386cdb3dbed55c3bf459b38
beardedsailor <raghav1392.cse18@chitkara.edu.in>
2020-05-14 17:01:01 +0530

    first day

---
+++ drum-kit-js(day-1)/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) {
+    return;
+  }
+  audio.play();
+  const val=`${e.keyCode}`;
+  
+   document.getElementById(val).style.border="4px solid yellow";
+  
+
+});
+window.addEventListener("keyup",function(d){
+  const va=`${d.keyCode}`;
+  document.getElementById(va).style.border="4px solid black";
+});
+
